b3e7d9f43c3c47714b76aabea18abb9d22c31431
Aderibigbe Dare <odunayo2009@gmail.com>
2021-09-30 18:35:46 -0500

    Add files via upload

---
+++ src/com/dare/Main.java
@@ -0,0 +1,33 @@
+package com.dare;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        ArrayList<Integer> numbers= new ArrayList<Integer>();
+
+         numbers.add(10);
+         numbers.add(20);
+         numbers.add(30);
+         numbers.add(40);
+         numbers.add(50);
+         numbers.add(3,33);
+         numbers.add(4,55);
+
+         numbers.indexOf(10);
+         Boolean ex= numbers.isEmpty();
+
+         numbers.set(2,4);
+
+
+
+     Boolean container =   numbers.contains(20);
+     if (container==true)
+         system.out.p
+
+         System.out.println(numbers);
+         System.out.println(numbers.contains(20));
+    }
+}
